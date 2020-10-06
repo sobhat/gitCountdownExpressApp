1f86aa4118ff6582f5672fa8849fe35c791ffdaf
Soumya Bhatt <sobhat@microsoft.com>
2020-10-05 20:51:46 -0400

    commit on 'master'

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var launchTime = new Date("12/29/2019 10:00").getTime();
 router.get('/', function(req, res, next) {
   var currentTime = new Date().getTime();
   var numberOfMilliseconds = parseInt(launchTime - currentTime);
-  res.render('index', { title: '(Main) Countdown to Launch', countDown: numberOfMilliseconds });;
+  res.render('index', { title: '(Main) Countdown to Launch ', countDown: numberOfMilliseconds });;
 });
 
 module.exports = router;
