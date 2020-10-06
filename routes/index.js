7640804d828b7b7968e71959879d5fc94e43c19f
Soumya Bhatt <sobhat@microsoft.com>
2020-10-05 20:47:49 -0400

    commit on 'branch-test'

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var launchTime = new Date("12/29/2019 10:00").getTime();
 router.get('/', function(req, res, next) {
   var currentTime = new Date().getTime();
   var numberOfMilliseconds = parseInt(launchTime - currentTime);
-  res.render('index', { title: '(Branch Test1) Countdown to Launch', countDown: numberOfMilliseconds });;
+  res.render('index', { title: '(Branch Test 1) Countdown to Launch', countDown: numberOfMilliseconds });;
 });
 
 module.exports = router;
